5aeb6e7abcb2e89bed90329d09f17f09b84ee60f
brandon <b97wright@gmail.com>
2022-09-04 14:07:19 -0400

    assignment 2b finished need qol

--- src/Assignment2B.java
+++ src/Assignment2B.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Assignment2B {
     public static void main (String[] args)
     {
         Scanner myObj = new Scanner(System.in);
-        int kf1, kf2, cfp, curX = 0, td;
+        double kf1, kf2, cfp, curX = 0, td;
 
         System.out.println("[Lerping!]");
         System.out.print("Enter the X coordinate for Keyframe #1: ");
-        kf1 = myObj.nextInt();
+        kf1 = myObj.nextDouble();
         System.out.print("Enter the X coordinate for Keyframe #2: ");
-        kf2 = myObj.nextInt();
+        kf2 = myObj.nextDouble();
         System.out.print("How many frames have passed? ");
-        cfp = myObj.nextInt();
+        cfp = myObj.nextDouble();
 
         td = kf2 - kf1;
 
-        curX = ((td * (cfp / 30)) + kf1);
+        curX = (kf1 + (td * (cfp/30)));
         System.out.println("At 30 FPS, their current X position would be " + curX + ".");
-        curX = (kf1 + (td * (cfp / 60)));
+        curX = (kf1 + (td * (cfp/60)));
         System.out.println("At 30 FPS, their current X position would be " + curX + ".");
         
     }
